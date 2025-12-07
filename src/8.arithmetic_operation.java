1ffa49d60748f55e36c2c88ed10a5576273fa2bd
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-07 18:17:13 +0530

    Fix commit message for arithmetic_operation
    Corrected the commit message for the arithmetic_operation program.

--- src/8.arithmetic_operation.java
+++ src/8.arithmetic_operation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// Correcting previous commit message for arithmetic_operation program
 class arithmetic_operation {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
